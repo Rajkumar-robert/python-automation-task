944912f8a92e42a328f706b1cd55a432846a5e94
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-04-17 22:49:36 +0530

    page load

--- convert.py
+++ convert.py
@@ -3,8 +3,8 @@ import pandas as pd
 from pymongo import MongoClient
 import math as m
 
-    
-# CONVERTING EXCEL TO DICTIONARY
+        
+    # CONVERTING EXCEL TO DICTIONARY
 def excel_to_json(excel_file):
     excel_data = pd.read_excel(excel_file)
     dict_data = excel_data.to_dict(orient='records')
@@ -14,17 +14,18 @@ def excel_to_json(excel_file):
 def insert_json_to_mongodb(dict_data):
     client = MongoClient(os.getenv("MONGODB_URL"))
     db = client.userdb
-    collection = db.userdata
+    collection = db.userData
 
 
 
     for doc in dict_data:
         # Extract tag values
 
-
+        
         tags = [str(doc[f'tag{i}']) for i in range(1, 6) if doc.get(f'tag{i}') is not None]
         
-        if(pd.isnull(tags[0])):
+        if(tags[0]!="nan"):
+
             # Assigning tag values to variables
             tag1 = tags[0] if tags[0] else ''
             tag2_values = tags[1].split(';') if len(tags) > 1 else []
@@ -39,19 +40,23 @@ def insert_json_to_mongodb(dict_data):
                     'module_id': tag2_values[1],
                     'activity_id': tag2_values[2],
                     'super_admin': tag3_values[0],
-                    'organization_id': tag3_values[1]
+                    'organization_id': tag3_values[1],
                 }
-
-                print(query)
+                update_record={"$set":{"report_data":doc}}
+          
                 # Check if a document with matching tags exists
-                existing_doc = collection.find_one(query)
+                existing_doc = collection.find_one({},query)
+                print(existing_doc)
                 if existing_doc:
+                    collection.find_one_and_update(query,update_record)
                     # Skip if a document with matching tags already exists
-                    print(f"Skipping document with tags: {tags}")
+                    # print(f"Skipping document with tags: {tags}")
+                    pass
                 else:
                     # Insert the document into the collection
-                    collection.insert_one(doc)
-                    print(f"Inserted document with tags: {tags}")
+                    # collection.insert_one(doc)
+                    # print(f"Inserted document with tags: {tags}")
+                    pass
             else:
                 # Skip if all tags are null
                 print("Skipping document with no tags")
@@ -59,5 +64,7 @@ def insert_json_to_mongodb(dict_data):
 
 excel_file_path = './candidates.xlsx'
 dict_data = excel_to_json(excel_file_path)
+
+# print(dict_data)
 
 insert_json_to_mongodb(dict_data)
--- main.py
+++ main.py
@@ -5,58 +5,55 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
-from dotenv import load_dotenv,dotenv_values
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
 
-
-win_driver_path="chromedriver-win64/chromedriver.exe"
-linux_driver_path="chromedriver-linux64/chromedriver"
+win_driver_path = "chromedriver-win64/chromedriver.exe"
+linux_driver_path = "chromedriver-linux64/chromedriver"
 
 service = Service(executable_path=win_driver_path)
 driver = webdriver.Chrome(service=service)
 userEmail = os.getenv("USER_EMAIL")
 password = os.getenv("PASSWORD")
-search_value="lambo"
+search_value = "lambo"
 
 driver.get(os.getenv("SITE_URL"))
 
-home_login_button=driver.find_element(By.CLASS_NAME,"wcgxB")
-home_login_button.click()
+try:
+    home_login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "wcgxB")))
+    home_login_button.click()
+    
 
-time.sleep(2)
-user_email=driver.find_element(By.NAME,"email")
-user_email.send_keys(userEmail)
+    user_email = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "email")))
+    user_email.send_keys(userEmail)
 
-time.sleep(1)
-user_password = driver.find_element(By.NAME,"password")
-user_password.send_keys(password)
+    user_password = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.NAME, "password")))
+    user_password.send_keys(password)
 
-login_button=driver.find_element(By.CLASS_NAME,"IUoDf")
-login_button.click()
+    login_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "IUoDf")))
+    login_button.click()
 
-time.sleep(2)
-search_field = driver.find_element(By.CLASS_NAME,"qoZU7")
-search_field.send_keys(search_value)
-search_field.send_keys(Keys.ENTER)
+    search_field = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "qoZU7")))
+    search_field.send_keys(search_value)
+    search_field.send_keys(Keys.ENTER)
 
-time.sleep(3)
+    for _ in range(76):
+        driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.TAB)
 
+    screen_width, screen_height = pyautogui.size()
+    pyautogui.click(screen_width / 2, screen_height / 2)
 
-for _ in range(76):
+    pyautogui.press('enter')
 
-    driver.find_element(By.TAG_NAME,'body').send_keys(Keys.TAB)
+    download_button = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "h3AWh")))
+    download_button.click()
 
-screen_width, screen_height = pyautogui.size()
-pyautogui.click(screen_width / 2, screen_height / 2)
+    time.sleep(30)
+    driver.quit()
 
-time.sleep(1)
-
-pyautogui.press('enter')
-
-
-download_button=driver.find_element(By.CLASS_NAME,"h3AWh")
-download_button.click()
-
-time.sleep(30)
-driver.quit()
+except Exception as e:
+    print("An error occurred:", e)
+    driver.quit()
