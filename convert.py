eb0160308c78d23dd9f1649c987aa4d4fd263cdb
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-04-17 12:41:16 +0530

    mongodb insert

--- convert.py
+++ convert.py
@@ -5,26 +5,32 @@ from pymongo import MongoClient
 
 def excel_to_json(excel_file):
     excel_data = pd.read_excel(excel_file) 
-    df = pd.DataFrame(excel_data)
-    json_data = df.to_json(orient='records')
+    json_data = excel_data.to_dict(orient='records')
+    print(type(json_data))
     return json_data
 
-excel_file_path = './candidates.xlsx' 
-json_data = excel_to_json(excel_file_path)
-print(json_data)
 
-
-def insert_json_to_mongodb(json_data,db_name,collection_name):
-    client = MongoClient()
+def insert_json_to_mongodb(json_data):
+    dict_data={
+        "name":"abc",
+        "age":"23"
+    }
     client = MongoClient("mongodb://localhost:27017/")
-    db=client[db_name]
-    collection=db[collection_name]
+    db=client.userdb
+    collection=db.userdata
     collection.insert_many(json_data)
+
+
 
 db_name = 'userdb'
 collection_name = 'userdata'
 
-insert_json_to_mongodb(json_data, db_name, collection_name)
+excel_file_path = './candidates.xlsx' 
+json_data = excel_to_json(excel_file_path)
+
+print(json_data)
+
+insert_json_to_mongodb(json_data)
     
 
 # def excel_to_jsonfile(excel_file, json_file):
