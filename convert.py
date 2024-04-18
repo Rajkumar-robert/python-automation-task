e4656893176a026608ac48c62b2b258fcd8d7f88
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-04-19 03:15:40 +0530

    demo site automation

--- convert.py
+++ convert.py
@@ -1,13 +1,20 @@
 import os
 import pandas as pd
 from pymongo import MongoClient
-import math as m
+from bson.objectid import ObjectId
 
         
     # CONVERTING EXCEL TO DICTIONARY
 def excel_to_json(excel_file):
+    # Read Excel file
     excel_data = pd.read_excel(excel_file)
+    
+    # Replace NaN values with empty strings
+    excel_data.fillna("", inplace=True)
+    
+    # Convert DataFrame to dictionary
     dict_data = excel_data.to_dict(orient='records')
+
     return dict_data
 
 # INSERTING DATA INTO DATABASE
@@ -17,50 +24,38 @@ def insert_json_to_mongodb(dict_data):
     collection = db.userData
 
 
-
+    i=0
     for doc in dict_data:
         # Extract tag values
-
+        i+=1
         
         tags = [str(doc[f'tag{i}']) for i in range(1, 6) if doc.get(f'tag{i}') is not None]
         
-        if(tags[0]!="nan"):
+        if(tags[0]!=""):
 
             # Assigning tag values to variables
             tag1 = tags[0] if tags[0] else ''
             tag2_values = tags[1].split(';') if len(tags) > 1 else []
             tag3_values = tags[2].split(';') if len(tags) > 2 else []
 
-            # Check if there are any non-null tags
-            if tag1:
-                # Query to check if any existing document has the same tags
-                query = {
-                    'learner_id': tag1,
-                    'program_id': tag2_values[0],
-                    'module_id': tag2_values[1],
-                    'activity_id': tag2_values[2],
-                    'super_admin': tag3_values[0],
-                    'organization_id': tag3_values[1],
-                }
-                update_record={"$set":{"report_data":doc}}
-          
-                # Check if a document with matching tags exists
-                existing_doc = collection.find_one({},query)
-                print(existing_doc)
-                if existing_doc:
-                    collection.find_one_and_update(query,update_record)
-                    # Skip if a document with matching tags already exists
-                    # print(f"Skipping document with tags: {tags}")
-                    pass
-                else:
-                    # Insert the document into the collection
-                    # collection.insert_one(doc)
-                    # print(f"Inserted document with tags: {tags}")
-                    pass
-            else:
-                # Skip if all tags are null
-                print("Skipping document with no tags")
-
+        
+            query = {
+                'learner_id': tag1,
+                'program_id': tag2_values[0],
+                'module_id': tag2_values[1],
+                'activity_id': tag2_values[2],
+                'super_admin': tag3_values[0],
+                'organization_id': tag3_values[1],
+            }
+            print(query)
+            update_record={"$set":{"report_data":doc}}
+        
+            # Check if a document with matching tags exists
+            existing_doc = collection.find_one({},query)
+            print(existing_doc)
+            if existing_doc:
+                # collection.update_one(,update_record)
+                pass
 
 excel_file_path = './candidates.xlsx'
 dict_data = excel_to_json(excel_file_path)
