883c032af53bd536b344516f4d5cb0c9151263a5
Rajkumar-robert <rajkumar09112003@gmail.com>
2024-04-19 17:48:43 +0530

    main.py

--- convert_main.py
+++ convert_main.py
@@ -6,10 +6,9 @@ from bson.objectid import ObjectId
 from dotenv import load_dotenv, dotenv_values
 
 load_dotenv()
+#find latest excel report
 def find_candidates_excel_file():
-    folder_path = os.getenv("XL_FILE_PATH")
-    print(folder_path)
-    downloads_path = os.path.abspath(folder_path)
+    downloads_path = os.path.abspath(os.getenv("XL_FILE_PATH"))
     latest_file = None
     latest_time = 0
     
@@ -20,68 +19,62 @@ def find_candidates_excel_file():
             if file_time > latest_time:
                 latest_file = file_path
                 latest_time = file_time
-    
+    print(latest_file)
     return latest_file
 
  
-# CONVERTING EXCEL TO DICTIONARY
+# converting excel to dictionary
 def excel_to_json(excel_file):
-    # Read Excel file
-    excel_data = pd.read_excel(excel_file)
-    
-    # Replace NaN values with empty strings
-    excel_data.fillna("", inplace=True)
-    
-    # Convert DataFrame to dictionary
-    dict_data = excel_data.to_dict(orient='records')
 
+    excel_data = pd.read_excel(excel_file)
+    excel_data.dropna(subset=['tag1'],inplace=True)
+    dict_data = excel_data.to_dict(orient='records')
+    print(len(dict_data))
     return dict_data
 
-# INSERTING DATA INTO DATABASE
+# inserting data into mongodb
 def insert_json_to_mongodb(dict_data):
     client = MongoClient(os.getenv("MONGODB_URL"))
-    db = client.userdb
-    collection = db.UserDataTest
+    db_name = os.getenv("DATABASE_NAME")
+    collection_name= os.getenv("COLLECTION_NAME")
+    db = client[db_name]
+    collection = db[collection_name]
 
     for doc in dict_data:
     # Extract tag values
         tags = [str(doc[f'tag{i}']) for i in range(1, 4) if doc.get(f'tag{i}') is not None]
+        # Assigning tag values to id
+        tag1 = tags[0] if tags[0] else ''
+        tag2_values = tags[1].split(';') if len(tags) > 1 else []
+        tag3_values = tags[2].split(';') if len(tags) > 2 else []
 
-        if tags[0] != "":
-        # Assigning tag values to variables
-            tag1 = tags[0] if tags[0] else ''
-            tag2_values = tags[1].split(';') if len(tags) > 1 else []
-            tag3_values = tags[2].split(';') if len(tags) > 2 else []
-
-            query = {
-                'learner_id': ObjectId(tag1),
-                'program_id': ObjectId(tag2_values[0]),
-                'module_id': ObjectId(tag2_values[1]),
-                'activity_id': ObjectId(tag2_values[2]),
-                'super_admin': ObjectId(tag3_values[0]),
-                'organization_id': ObjectId(tag3_values[1]),
-            }
-            update_record = {"$set": {"report_data": doc}}
-            # Check if a document with matching tags exists
-            existing_doc = collection.find_one(query)
-       
-            if existing_doc:
-               
-                report_data=existing_doc['report_data']
-                if 'UniqueId' in report_data:
-                   
-                    
-                    if report_data['UniqueId'] == doc['UniqueId']:
-                        print("same unique id is existing")
-                        continue  # Skip if the existing document has the same UniqueId
-             
-                object_id = ObjectId(existing_doc['_id'])
-                if object_id:
-                    collection.find_one_and_update({'_id': object_id}, update_record)
-                    print("Inserted document successfully!!")
-                   
-            else:
-                print("No document exists with similar id")
+        query = {
+            'learner_id': ObjectId(tag1),
+            'program_id': ObjectId(tag2_values[0]),
+            'module_id': ObjectId(tag2_values[1]),
+            'activity_id': ObjectId(tag2_values[2]),
+            'super_admin': ObjectId(tag3_values[0]),
+            'organization_id': ObjectId(tag3_values[1]),
+        }
+        update_record = {"$set": {"report_data": doc}}
+        
+        # Check if a document with matching tags exists
+        existing_doc = collection.find_one(query)
+        if existing_doc:
+            report_data=existing_doc['report_data']
+            #checking uniqueId 
+            if 'UniqueId' in report_data and report_data['UniqueId'] == doc['UniqueId']:
+                    print("same uniqueId is detected")
+                    continue  # Skip if the existing document has the same UniqueId
+            
+            object_id = ObjectId(existing_doc['_id'])
+            if object_id:
+                #inserting report_data into document
+                collection.find_one_and_update({'_id': object_id}, update_record)
+                print("Inserted document successfully!!")
+                
+        else:
+            print("No document exists with similar id")
                 
 def delete_file(file_path):
     try:
@@ -101,6 +94,8 @@ def main():
         insert_json_to_mongodb(dict_data)
         delete_file(excel_file_path)
         print("Excel file deleted.")
+    else:
+        print("No file detected")
         
 if __name__ == "__main__":
     main()
